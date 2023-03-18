e76fe4d58f39e2a3b434cfd4b4169a9b983a6cf2
enaercina <125195785+enaercina@users.noreply.github.com>
2023-03-18 15:04:31 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,68 @@
-# python3
+def heapify(arr, N, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
+    swaps = []
 
+    # See if left child of root exists and is
+    # greater than root
+    if l < N and arr[largest] < arr[l]:
+        largest = l
+
+    # See if right child of root exists and is
+    # greater than root
+    if r < N and arr[largest] < arr[r]:
+        largest = r
+
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+        swaps.append((i, largest))
+        total_swaps = 1
+
+        # Heapify the root.
+        sub_swaps, sub_total_swaps = heapify(arr, N, largest)
+        swaps.extend(sub_swaps)
+        total_swaps += sub_total_swaps
+
+    return swaps, total_swaps
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    total_swaps = 0
 
+    # Build a maxheap.
+    for i in range(len(data) // 2 - 1, -1, -1):
+        sub_swaps, sub_total_swaps = heapify(data, len(data), i)
+        swaps.extend(sub_swaps)
+        total_swaps += sub_total_swaps
 
-    return swaps
+    # One by one extract elements
+    for i in range(len(data) - 1, 0, -1):
+        data[i], data[0] = data[0], data[i]  # swap
+        swaps.append((0, i))
+        sub_swaps, sub_total_swaps = heapify(data, i, 0)
+        swaps.extend(sub_swaps)
+        total_swaps += sub_total_swaps
+
+    return data, swaps, total_swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
     # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
+    # checks if length of data is the same as the said length
     assert len(data) == n
 
-    # calls function to assess the data 
+    # calls function to assess the data
     # and give back all swaps
-    swaps = build_heap(data)
+    data, swaps, total_swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
+    # output how many swaps were made
+    print("Number of swaps made: ", total_swaps)
 
     # output all swaps
-    print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
-if __name__ == "__main__":
-    main()
