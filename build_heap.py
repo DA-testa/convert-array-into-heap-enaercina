f3412b9cd28ecc4f9e02283be01f767c3d9b9841
enaercina <125195785+enaercina@users.noreply.github.com>
2023-03-18 16:43:56 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -20,12 +20,27 @@ def heapify(a):
     num_swaps = len(swaps)
     return num_swaps, swaps
 def main():
-    i=input()
-    n = int(input())
-    a = list(map(int, input().split()))
-    assert len(a) == n
-    num_swaps, swaps = heapify(a)
-
+    ievade = input("Input I or F:  ")
+    if "I" in ievade:
+        n = int(input())
+        a = list(map(int, input().split()))
+        assert len(a) == n
+        num_swaps, swaps = heapify(a)
+    elif "F" in ievade:
+        fails = "./tests/" + input("Input filename(04): ")
+        if "a" in fails:
+            print("wrong file name")
+            return
+        with open(fails) as f:
+            n = int(f.readline())
+            a = list(map(int, f.readline().split()))
+            assert len(a) == n
+            num_swaps, swaps = heapify(a)
+    else:
+        print("wrong input")
+        return
+    swaps_l = len(swaps)
+    assert swaps_l < 4 * len(a)
     print(num_swaps)
     for i, j in swaps:
         print(i, j)
