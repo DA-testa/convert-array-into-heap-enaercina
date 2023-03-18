05265ce4b107f84c2a689f81b52cdef3a5972f81
enaercina <125195785+enaercina@users.noreply.github.com>
2023-03-18 16:37:29 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -19,18 +19,17 @@ def heapify(a):
 
     num_swaps = len(swaps)
     return num_swaps, swaps
-if __name__ == "__main__":
-    I=input()
+def main():
+    i=input()
     n = int(input())
     a = list(map(int, input().split()))
-
+    assert len(a) == n
     num_swaps, swaps = heapify(a)
 
-#    print(num_swaps)
-#    for i, j in swaps:
-#        print(i, j)
-    result = str(num_swaps)+" "
-    for sublist in swaps:
-        for element in sublist:
-            result = result + str(element) + " "
-    print(result)
+    print(num_swaps)
+    for i, j in swaps:
+        print(i, j)
+
+
+if __name__ == "__main__":
+    main()
